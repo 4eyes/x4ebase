b482d75159e3955e72e735bf0baeb0dfdd53ed60
philipp <philipp@4eyes.ch>
2015-08-13 17:39:28 +0200

    Added generic filter repository and filter template class to x4ebase

---
+++ Classes/Template/FilterTemplate.php
@@ -0,0 +1,184 @@
+<?php
+namespace X4E\X4ebase\Template;
+
+/***************************************************************
+ *
+ *  Copyright notice
+ *
+ *  (c) 2015 Philipp <philipp@4eyes.ch>, 4eyes GmbH
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+class FilterTemplate {
+    /** @var array $filterArray All Filter keys and their selected filters in an associative array
+     * Might be:
+         $filterArray = array(
+            'type'         => array(1,3);
+            'languages'    => array(4,5,6);
+            'tags'         => array(2);
+         );
+     */
+    protected $filterArray = array();
+    /** @var array $searchStrings The Words a User searches for as an Array-List
+     * Might be:
+        $searchStrings = array('Hello','World');
+     */
+    protected $searchStrings = array();
+    /** @var array $searchableParameters Associative Array: Key is the Parameter a user should be able to search in with $searchStrings, Value is the kind of search (like, contains, ...)
+     * Possible Array:
+         protected $searchableParameters = array(
+             'name'             => 'like',
+             'description'      => 'like',
+             'authors.name'     => 'like',
+             'languages.name'   => 'like',
+             'tags'             => 'contains',
+         );
+     */
+    protected $searchableParameters = array();
+    /** @var array $filterMethods Associative Array: Key is the Parameter of the object, Value is the kind of filter method (equals, contains, ...)
+     * Possible Array:
+         protected $filterMethods = array(
+              'name'        => 'equals',
+              'type'        => 'equals',
+              'languages'   => 'contains',
+              'description' => 'equals',
+              'authors'     => 'contains',
+              'tags'        => 'contains',
+         );
+     */
+    protected $filterMethods = array();
+
+    /**
+     * @return array
+     */
+    public function getFilterArray()
+    {
+        return $this->filterArray;
+    }
+
+    /**
+     * @param array $filterArray
+     */
+    public function setFilterArray($filterArray)
+    {
+        $this->filterArray = $filterArray;
+    }
+
+    /**
+     * @return array
+     */
+    public function getSearchStrings()
+    {
+        return $this->searchStrings;
+    }
+
+    /**
+     * @param array $searchStrings
+     */
+    public function setSearchStrings($searchStrings)
+    {
+        $this->searchStrings = $searchStrings;
+    }
+
+    /**
+     * @return array
+     */
+    public function getSearchableParameters()
+    {
+        return $this->searchableParameters;
+    }
+
+    /**
+     * @param array $searchableParameters
+     */
+    public function setSearchableParameters($searchableParameters)
+    {
+        $this->searchableParameters = $searchableParameters;
+    }
+
+    /**
+     * @param String $searchableParameter A parameter the user can search in
+     */
+    public function addSearchableParameter($searchableParameter) {
+        if(!in_array($searchableParameter, $this->searchableParameters))
+            $this->searchableParameters[] = $searchableParameter;
+    }
+
+    /**
+     * @param array $searchableParameters Multiple parameters a User can search in
+     */
+    public function addMultipleSearchableParameters($searchableParameters) {
+        foreach($searchableParameters as $searchableParameter)
+            $this->addSearchableParameter($searchableParameter);
+    }
+
+    /**
+     * @param String $searchableParameter A parameter the user should not be able to search in anymore
+     */
+    public function removeSearchableParameter($searchableParameter) {
+        if(($key = array_search($searchableParameter, $this->searchableParameters)) !== false)
+            unset($this->searchableParameters[$key]);
+    }
+
+    /**
+     * @param String $parameterName The filter-key
+     * @param Array $parameterValue All selected Filters of the filter-key
+     */
+    public function addFilterToFilterArray($parameterName, $parameterValue) {
+        if(!empty($parameterValue))
+            $this->filterArray[$parameterName] = $parameterValue;
+    }
+
+    /**
+     * @param String $filterName The filter-key
+     */
+    public function removeFilterToFilterArray($filterName) {
+        unset($this->filterArray[$filterName]);
+    }
+
+    /**
+     * @param String $search the Search String (normal Text, spaces are used as separators)
+     */
+    public function setSearchStringsFromSpaceSeparatedList($search) {
+        $this->searchStrings = preg_split('/ /', $search, -1, PREG_SPLIT_NO_EMPTY);
+    }
+
+    /**
+     * @param String $parameter The parameter of the object
+     * @return String
+     */
+    public function getFilterMethodForParameter($parameter) {
+        return $this->filterMethods[$parameter];
+    }
+
+    /**
+     * @param array $filterMethods Sets the filter methods
+     */
+    public function setFilterMethods($filterMethods) {
+        $this->filterMethods = $filterMethods;
+    }
+
+    /**
+     * @return array Returns the filter methods
+     */
+    public function getFilterMethods() {
+        return $this->filterMethods;
+    }
+}
