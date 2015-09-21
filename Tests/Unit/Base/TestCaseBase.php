5777e574db9805f2880678c2f2465a81198c82b2
philipp <philipp@4eyes.ch>
2015-09-21 17:10:48 +0200

    AUSBILDUNG-6 - Test-Base for Typo3 6.2. PHP5.3 does not support ::class

---
+++ Tests/Unit/Base/TestCaseBase.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace X4E\X4ebase\Tests\Unit\Base;
+
+	/* * *************************************************************
+	 *  Copyright notice
+	 *
+	 *  (c) 2015 Philipp Seßner <philipp@4eyes.ch>, 4eyes GmbH
+	 *
+	 *  All rights reserved
+	 *
+	 *  This script is part of the TYPO3 project. The TYPO3 project is
+	 *  free software; you can redistribute it and/or modify
+	 *  it under the terms of the GNU General Public License as published by
+	 *  the Free Software Foundation; either version 2 of the License, or
+	 *  (at your option) any later version.
+	 *
+	 *  The GNU General Public License can be found at
+	 *  http://www.gnu.org/copyleft/gpl.html.
+	 *
+	 *  This script is distributed in the hope that it will be useful,
+	 *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	 *  GNU General Public License for more details.
+	 *
+	 *  This copyright notice MUST APPEAR in all copies of the script!
+	 * ************************************************************* */
+
+/**
+ * Test class for class \X4E\X4ebase\XClasses\Persistence\Generic\Typo3QuerySettings
+ *
+ * @version $Id$
+ * @copyright Copyright belongs to the respective authors
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ *
+ * @author Philipp Seßner <philipp@4eyes.ch>
+ */
+class TestCaseBase extends \TYPO3\CMS\Core\Tests\UnitTestCase {
+
+	/** @var  \PHPUnit_Framework_MockObject_MockObject|\TYPO3\CMS\Core\Tests\AccessibleObjectInterface */
+	protected $subject;
+
+	public function setUp() {
+		if (function_exists('xdebug_disable')) {
+			xdebug_disable();
+		}
+	}
+
+	public function tearDown() {
+		unset($this->subject);
+		if (function_exists('xdebug_enable')) {
+			xdebug_enable();
+		}
+	}
+
+	protected function mockSubject() {
+		$methods = func_get_args();
+		if (empty($methods)) {
+			$methods = array('dummy');
+		}
+		$this->subject = $this->getAccessibleSubjectInstance($methods);
+	}
+
+	/**
+	 * @return string
+	 */
+	protected function getSubjectClassName() {
+		//get class name of viewHelperTest
+		$class = get_class($this);
+		//trim 'test' off the viewHelperTest
+		$class = substr($class, 0, -4);
+		//delete 'Tests\Unit\' from namespace
+		$class = str_replace('Tests\\Unit\\', '', $class);
+		return $class;
+	}
+
+	/**
+	 * @param array $methods
+	 *
+	 * @return \PHPUnit_Framework_MockObject_MockObject|\TYPO3\CMS\Core\Tests\AccessibleObjectInterface
+	 */
+	protected function getAccessibleSubjectInstance(array $methods = array('dummy')) {
+		return $this->getAccessibleMock(
+			$this->getSubjectClassName(),
+			$methods,
+			array(),
+			'',
+			FALSE
+		);
+	}
+}
---
+++ Tests/Unit/Base/ViewHelperTestBase.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace X4E\X4ebase\Tests\Unit\Base;
+
+/* * *************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2015 Philipp Seßner <philipp@4eyes.ch>, 4eyes GmbH
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ * ************************************************************* */
+
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+/**
+ * Base class for all repository test classes.
+ *
+ * @version $Id$
+ * @copyright Copyright belongs to the respective authors
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ *
+ * @author Philipp Seßner <philipp@4eyes.ch>
+ */
+class ViewHelperTestBase extends \X4E\X4ebase\Tests\Unit\Base\TestCaseBase {
+
+	/** @var \TYPO3\CMS\Fluid\Core\ViewHelper\ViewHelperVariableContainer */
+	protected $viewHelperVariableContainer;
+	/** @var \TYPO3\CMS\Fluid\Core\ViewHelper\TemplateVariableContainer */
+	protected $templateVariableContainer;
+	/** @var \TYPO3\CMS\Extbase\Mvc\Web\Routing\UriBuilder */
+	protected $uriBuilder;
+	/** @var \TYPO3\CMS\Extbase\Mvc\Controller\ControllerContext */
+	protected $controllerContext;
+	/** @var \TYPO3\CMS\Fluid\Core\ViewHelper\TagBuilder */
+	protected $tagBuilder;
+	/** @var \TYPO3\CMS\Fluid\Core\ViewHelper\Arguments */
+	protected $arguments;
+	/** @var \TYPO3\CMS\Extbase\Mvc\Web\Request */
+	protected $request;
+	/** @var \TYPO3\CMS\Fluid\Core\Rendering\RenderingContext */
+	protected $renderingContext;
+	/** @var \TYPO3\CMS\Extbase\Mvc\Controller\MvcPropertyMappingConfigurationService */
+	protected $mvcPropertyMapperConfigurationService;
+
+	/** @var  \PHPUnit_Framework_MockObject_MockObject|\TYPO3\CMS\Core\Tests\AccessibleObjectInterface|AbstractViewHelper */
+	protected $subject;
+
+	public function setUp() {
+		parent::setUp();
+		$this->setUpDependencies();
+		$this->mockSubject('registerArguments');
+	}
+
+	public function tearDown() {
+		unset($this->viewHelperVariableContainer);
+		unset($this->templateVariableContainer);
+		unset($this->uriBuilder);
+		unset($this->controllerContext);
+		unset($this->tagBuilder);
+		unset($this->arguments);
+		unset($this->request);
+		unset($this->renderingContext);
+		unset($this->mvcPropertyMapperConfigurationService);
+		parent::tearDown();
+	}
+
+	protected function setUpDependencies() {
+		$this->viewHelperVariableContainer = $this->getMock('\TYPO3\CMS\Fluid\Core\ViewHelper\ViewHelperVariableContainer');
+		$this->templateVariableContainer = $this->getMock('\TYPO3\CMS\Fluid\Core\ViewHelper\TemplateVariableContainer');
+		$this->uriBuilder = $this->getMock('\TYPO3\CMS\Extbase\Mvc\Web\Routing\UriBuilder');
+		$this->uriBuilder->expects($this->any())->method('reset')->willReturn($this->uriBuilder);
+		$this->uriBuilder->expects($this->any())->method('setArguments')->willReturn($this->uriBuilder);
+		$this->uriBuilder->expects($this->any())->method('setSection')->willReturn($this->uriBuilder);
+		$this->uriBuilder->expects($this->any())->method('setFormat')->willReturn($this->uriBuilder);
+		$this->uriBuilder->expects($this->any())->method('setCreateAbsoluteUri')->willReturn($this->uriBuilder);
+		$this->uriBuilder->expects($this->any())->method('setAddQueryString')->willReturn($this->uriBuilder);
+		$this->uriBuilder->expects($this->any())->method('setArgumentsToBeExcludedFromQueryString')->willReturn($this->uriBuilder);
+		$this->uriBuilder->expects($this->any())->method('setLinkAccessRestrictedPages')->willReturn($this->uriBuilder);
+		$this->uriBuilder->expects($this->any())->method('setTargetPageUid')->willReturn($this->uriBuilder);
+		$this->uriBuilder->expects($this->any())->method('setTargetPageType')->willReturn($this->uriBuilder);
+		$this->uriBuilder->expects($this->any())->method('setNoCache')->willReturn($this->uriBuilder);
+		$this->uriBuilder->expects($this->any())->method('setUseCacheHash')->willReturn($this->uriBuilder);
+		$this->uriBuilder->expects($this->any())->method('setAddQueryStringMethod')->willReturn($this->uriBuilder);
+		$this->request = $this->getMock('\TYPO3\CMS\Extbase\Mvc\Web\Request');
+		$this->controllerContext = $this->getMock('\TYPO3\CMS\Extbase\Mvc\Controller\ControllerContext', array(), array(), '', FALSE);
+		$this->controllerContext->expects($this->any())->method('getUriBuilder')->willReturn($this->uriBuilder);
+		$this->controllerContext->expects($this->any())->method('getRequest')->willReturn($this->request);
+		$this->tagBuilder = $this->getMock('\TYPO3\CMS\Fluid\Core\ViewHelper\TagBuilder');
+		$this->arguments = array();
+		$this->renderingContext = $this->getAccessibleMock('\TYPO3\CMS\Fluid\Core\Rendering\RenderingContext', array('dummy'));
+		$this->renderingContext->injectTemplateVariableContainer($this->templateVariableContainer);
+		$this->renderingContext->_set('viewHelperVariableContainer', $this->viewHelperVariableContainer);
+		$this->renderingContext->setControllerContext($this->controllerContext);
+		$this->mvcPropertyMapperConfigurationService = $this->getAccessibleMock('\TYPO3\CMS\Extbase\Mvc\Controller\MvcPropertyMappingConfigurationService', array('dummy'));
+	}
+
+	/**
+	 * @param \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper $viewHelper
+	 * @return void
+	 */
+	protected function injectDependenciesIntoViewHelper(\TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper $viewHelper) {
+		$viewHelper->setRenderingContext($this->renderingContext);
+		$viewHelper->setArguments($this->arguments);
+		if ($viewHelper instanceof \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractTagBasedViewHelper) {
+			$viewHelper->_set('tag', $this->tagBuilder);
+		}
+	}
+
+	protected function mockSubject() {
+		call_user_func_array('parent::' . __FUNCTION__, func_get_args());
+		$this->injectDependenciesIntoViewHelper($this->subject);
+	}
+
+	protected function initializeArgumentsTest($arguments, $tagAttributes=0) {
+		$this->mockSubject('registerArgument');
+		$this->checkIfRegisterArgumentsGotCalledNTimes($arguments);
+		$this->checkIfRegisterTagAttributeGotCalledNTimes($tagAttributes);
+		$this->subject->initializeArguments();
+	}
+
+	protected function checkIfRegisterArgumentsGotCalledNTimes($n) {
+		$this->subject
+			->expects($this->exactly($n))
+			->method('registerArgument');
+	}
+
+	protected function checkIfRegisterTagAttributeGotCalledNTimes($n) {
+		$this->subject
+			->expects($this->exactly($n))
+			->method('registerTagAttribute');
+	}
+
+}
