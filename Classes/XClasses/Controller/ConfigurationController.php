8e683f21c341cc55bcf89c3a514c3e1136504e13
markus <markus@4eyes.ch>
2016-04-16 19:06:27 +0200

    IMPROVE-409: added commands to controller and xclass to enable cli configuration of extension

---
+++ Classes/XClasses/Controller/ConfigurationController.php
@@ -0,0 +1,28 @@
+<?php
+namespace X4E\X4ebase\XClasses\Controller;
+
+    /**
+     * This file is part of the TYPO3 CMS project.
+     *
+     * It is free software; you can redistribute it and/or modify it under
+     * the terms of the GNU General Public License, either version 2
+     * of the License, or any later version.
+     *
+     * For the full copyright and license information, please read the
+     * LICENSE.txt file that was distributed with this source code.
+     *
+     * The TYPO3 project - inspiring people to share!
+     */
+
+/**
+ * CommandController for working with extension management through CLI/scheduler
+ *
+ * @author Claus Due <claus@namelesscoder.net>
+ */
+class ConfigurationController extends \TYPO3\CMS\Extensionmanager\Controller\ConfigurationController {
+
+    public function saveConfiguration(array $config, $extensionKey) {
+        parent::saveConfiguration($config, $extensionKey);
+    }
+
+}
