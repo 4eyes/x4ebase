d72a4925ada2cf86dba43bb92354d8dda76e6642
Michel Georgy <michel@4eyes.ch>
2016-07-14 16:59:15 +0200

    DEVOPS-12: Implemented own exception handler so sentry gets catched content exception

---
+++ Classes/ContentObject/Exception/SentryProductionExceptionHandler.php
@@ -0,0 +1,66 @@
+<?php
+namespace X4E\X4ebase\ContentObject\Exception;
+
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2016 Michel Georgy <michel@4eyes.ch>, 4eyes GmbH
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+use \TYPO3\CMS\Frontend\ContentObject\Exception\ProductionExceptionHandler;
+use \TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
+use \TYPO3\CMS\Core\Utility\GeneralUtility;
+use \Lemming\SentryClient\Client;
+
+/**
+ *
+ *
+ * @package x4ebase
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ */
+class SentryProductionExceptionHandler extends ProductionExceptionHandler{
+    protected function logException(\Exception $exception, $errorMessage, $code) {
+        parent::logException($exception, $errorMessage, $code);
+
+        if (isset($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['sentry_client'])) {
+            $configuration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['sentry_client']);
+            if (isset($configuration['dsn']) && $configuration['dsn'] != '') {
+
+                if (isset($configuration['productionOnly']) && (bool)$configuration['productionOnly'] === TRUE) {
+                    if (GeneralUtility::getApplicationContext()->isProduction()) {
+                        $this->handleSentryException($exception);
+                    }
+                } else {
+                    $this->handleSentryException($exception);
+                }
+            }
+        }
+    }
+
+    /**
+     * @param \Exception $exception
+     */
+    function handleSentryException($exception) {
+        $client = new Client();
+        $errorHandler = new \Raven_ErrorHandler($client, TRUE);
+        $errorHandler->handleException($exception);
+    }
+}
--- Classes/Controller/ContentExceptionTestController.php
+++ Classes/Controller/ContentExceptionTestController.php
@@ -1,29 +1,32 @@
 <?php
 namespace X4E\X4ebase\Controller;
 
-    /***************************************************************
-     *  Copyright notice
-     *
-     *  (c) 2016 Michel Georgy <michel@4eyes.ch>, 4eyes GmbH
-     *
-     *  All rights reserved
-     *
-     *  This script is part of the TYPO3 project. The TYPO3 project is
-     *  free software; you can redistribute it and/or modify
-     *  it under the terms of the GNU General Public License as published by
-     *  the Free Software Foundation; either version 3 of the License, or
-     *  (at your option) any later version.
-     *
-     *  The GNU General Public License can be found at
-     *  http://www.gnu.org/copyleft/gpl.html.
-     *
-     *  This script is distributed in the hope that it will be useful,
-     *  but WITHOUT ANY WARRANTY; without even the implied warranty of
-     *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-     *  GNU General Public License for more details.
-     *
-     *  This copyright notice MUST APPEAR in all copies of the script!
-     ***************************************************************/
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2016 Michel Georgy <michel@4eyes.ch>, 4eyes GmbH
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
+use \TYPO3\CMS\Core\Exception;
 
 /**
  *
@@ -31,12 +34,12 @@ namespace X4E\X4ebase\Controller;
  * @package x4ebase
  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
  */
-class ContentExceptionTestController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController {
+class ContentExceptionTestController extends ActionController {
 
     public function contentAction(){
     }
 
     public function exceptionAction(){
-        throw new \TYPO3\CMS\Core\Exception('Test Exception to Test contentExceptionHandler', 1468489285);
+        throw new Exception('Test Exception to Test contentExceptionHandler', 1468489285);
     }
 }
