aee6856fb7825f04f00d95dac1b1b7ef26f23cf0
Alessandro Bellafronte <alessandro@4eyes.ch>
2016-11-21 10:38:51 +0100

    Refactored FlashMessageQueue handling, to support TYPO3 versions >= 7.6

--- Classes/Controller/EmailQueueCommandController.php
+++ Classes/Controller/EmailQueueCommandController.php
@@ -24,6 +24,9 @@ namespace X4e\X4ebase\Controller;
  *
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
+use TYPO3\CMS\Core\Messaging\FlashMessage;
+use TYPO3\CMS\Core\Messaging\FlashMessageQueue;
+use TYPO3\CMS\Core\Messaging\FlashMessageService;
 
 /**
  *
@@ -31,79 +34,99 @@ namespace X4e\X4ebase\Controller;
  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
  *
  */
-class EmailQueueCommandController extends \TYPO3\CMS\Extbase\Mvc\Controller\CommandController {
+class EmailQueueCommandController extends \TYPO3\CMS\Extbase\Mvc\Controller\CommandController
+{
 
-	/**
-	 * emailLogRepository
-	 *
-	 * @var \X4e\X4ebase\Domain\Repository\EmailLogRepository
-	 * @inject
-	 */
-	protected $emailLogRepository;
+    /**
+     * emailLogRepository
+     *
+     * @var \X4e\X4ebase\Domain\Repository\EmailLogRepository
+     * @inject
+     */
+    protected $emailLogRepository;
 
-	/**
-	 * The settings.
-	 * @var array
-	 */
-	protected $settings = array();
+    /**
+     * The settings.
+     * @var array
+     */
+    protected $settings = [];
 
-	/**
-	 * @var int
-	 */
-	protected $mailsPerRun = 30;
+    /**
+     * @var int
+     */
+    protected $mailsPerRun = 30;
 
-	/**
-	 * @var \TYPO3\CMS\Extbase\Configuration\ConfigurationManager
-	 * @inject
-	 */
-	protected $configurationManager;
+    /**
+     * @var \TYPO3\CMS\Extbase\Configuration\ConfigurationManager
+     * @inject
+     */
+    protected $configurationManager;
 
+    /**
+     * Command which replicates DB
+     *
+     * @return void
+     */
+    public function processEmailQueueCommand()
+    {
+        $this->settings = $this->configurationManager->getConfiguration(
+            \TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface::CONFIGURATION_TYPE_SETTINGS,
+            'x4ebase',
+            'tx_x4ebase'
+        );
+        $mailsInQueue = $this->emailLogRepository->findByQueued(true);
 
-	/**
-	 * Command which replicates DB
-	 *
-	 * @return void
-	 */
-	public function processEmailQueueCommand() {
-		$this->settings = $this->configurationManager->getConfiguration(\TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface::CONFIGURATION_TYPE_SETTINGS, 'x4ebase', 'tx_x4ebase');
-		$mailsInQueue = $this->emailLogRepository->findByQueued(TRUE);
+        if ($this->settings['emailQueueCommandController']['mailsPerRun']) {
+            $mailsPerRun = $this->settings['emailQueueCommandController']['mailsPerRun'];
+        } else {
+            $mailsPerRun = $this->mailsPerRun;
+        }
 
-		if($this->settings['emailQueueCommandController']['mailsPerRun']){
-			$mailsPerRun = $this->settings['emailQueueCommandController']['mailsPerRun'];
-		} else {
-			$mailsPerRun = $this->mailsPerRun;
-		}
+        $i = 0;
+        foreach ($mailsInQueue as $mail) {
+            if ($i >= $mailsPerRun) {
+                break;
+            }
 
-		$i = 0;
-		foreach($mailsInQueue as $mail) {
-			if ($i >= $mailsPerRun) {
-				break;
-			}
+            $sent = \X4e\X4ebase\Utility\EmailUtility::sendQueuedEmail($mail);
+            if ($sent) {
+                $this->addFlashMessage(
+                    $mail->getRecipient(),
+                    'mail sent'
+                );
+            } else {
+                $this->addFlashMessage(
+                    'Mail to ' . $mail->getRecipient() . ' not send. Trying in the next run',
+                    'mail not sent',
+                    FlashMessage::INFO
+                );
+            }
+            $i++;
+        }
 
-			$sent = \X4e\X4ebase\Utility\EmailUtility::sendQueuedEmail($mail);
-			if ($sent) {
-				$this->addFlashMessage($mail->getRecipient(), 'mail sent');
-			} else {
-				$this->addFlashMessage('Mail to ' . $mail->getRecipient() .' not send. Trying in the next run', 'mail not sent', \TYPO3\CMS\Core\Messaging\FlashMessage::INFO);
-			}
-			$i++;
-		}
+        return true;
+    }
 
-		return TRUE;
-	}
-
-	/**
-	 * Adds a flash message to the queue
-	 *
-	 * @param string $message
-	 * @param string $title
-	 * @param integer $status
-	 */
-	function addFlashMessage($message, $title, $status = \TYPO3\CMS\Core\Messaging\FlashMessage::OK) {
-		$flashMessage = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-						't3lib_FlashMessage', $message, $title, $status
-		);
-		\TYPO3\CMS\Core\Messaging\FlashMessageQueue::addMessage($flashMessage);
-	}
+    /**
+     * Adds a flash message to the queue
+     *
+     * @param string $message
+     * @param string $title
+     * @param int $status
+     */
+    public function addFlashMessage($message, $title, $status = FlashMessage::OK)
+    {
+        /** @var $flashMessage FlashMessage */
+        $flashMessage = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+            FlashMessage::class,
+            $message,
+            $title,
+            $status
+        );
+        /** @var FlashMessageService $flashMessageService */
+        $flashMessageService = $this->objectManager->get(FlashMessageService::class);
+        /** @var FlashMessageQueue $defaultFlashMessageQueue */
+        $defaultFlashMessageQueue = $flashMessageService->getMessageQueueByIdentifier();
+        $defaultFlashMessageQueue->enqueue($flashMessage);
+    }
 }
-?>
